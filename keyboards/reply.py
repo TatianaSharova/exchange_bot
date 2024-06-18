1b80948926b571cf39213a42ddb14d699ef46a50
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-06-18 14:09:00 +0400

    add keyboards for subscription

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -18,13 +18,25 @@ for crypto in crypto_symbols:
     crypto_kb.add(types.KeyboardButton(text=crypto))
 crypto_kb.adjust(3)
 
-help_kb = types.ReplyKeyboardMarkup(
+help_kb = ReplyKeyboardBuilder()
+commands = ['Криптовалюты', 'Подписка на крипту', 'Отменить подписку', 'Изменить подписку']
+for command in commands:
+    help_kb.add(types.KeyboardButton(text=command))
+help_kb.adjust(2)
+
+subscription_kb = ReplyKeyboardBuilder()
+subs = ['Подписаться на крипту', 'Отменить подписку', 'Изменить подписку']
+for sub in subs:
+    help_kb.add(types.KeyboardButton(text=command))
+subscription_kb.adjust(2)
+
+value_kb = types.ReplyKeyboardMarkup(
     keyboard=[
         [
-            types.KeyboardButton(text="Криптовалюты"),
-            types.KeyboardButton(text="Подписка на крипту")
+            types.KeyboardButton(text="MIN"),
+            types.KeyboardButton(text="MAX")
         ]
     ],
     resize_keyboard=True,
-    # one_time_keyboard=True
+    one_time_keyboard=True
     )
