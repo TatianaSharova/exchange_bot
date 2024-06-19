33412a9484623805956b02b01288424b72c007f9
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-06-19 14:33:29 +0400

    deleted unuseful handlers

--- handlers/user.py
+++ handlers/user.py
@@ -1,5 +1,5 @@
 from aiogram import F, Router, types
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from keyboards import reply
 from .utils import get_crypto_price
 from aiogram.enums import ParseMode
@@ -12,13 +12,16 @@ user_canal_router = Router()
 async def send_welcome(message: types.Message) -> types.Message:
     await message.answer(
         "Привет! Я бот, который может предоставить информацию о стоимости выбранной криптовалюты в USD.\n"
+        "\n"
         "Нажми на кнопку <Криптовалюты>, чтобы перейти к нужной криптовалюте.\n"
+        "\n"
         "Если хочешь ознакомиться со всеми функциями бота, нажми на кнопку <Помощь>."
         )
 
     await message.answer("Выберите опцию:", reply_markup=reply.start_kb)
 
 
+@user_canal_router.message(Command("check_price"))
 @user_canal_router.message(F.text.lower() == "криптовалюты")
 async def offer_famous_crypto(message: types.Message) -> types.Message:
     await message.reply(
@@ -33,53 +36,28 @@ async def offer_famous_crypto(message: types.Message) -> types.Message:
         reply_markup=reply.crypto_kb.as_markup(resize_keyboard=True),
     )
 
-
+@user_canal_router.message(Command("help"))
 @user_canal_router.message(F.text.lower() == "помощь")
 async def get_help(message: types.Message) -> types.Message:
     await message.answer(
-        "Этот бот может:\n"
+        "Я могу:\n"
         "1. Посмотреть стоимость выбранной крипты в USD.\n"
         "Для этого нажми кнопку <Криптовалюты>.\n"
+        "\n"
         "2. Начать отслеживать курс отдельных криптовалют, "
         "присылая уведомления, когда курс достигает выбранных вами значений.\n"
-        "Для этого нажми на кнопку <Подписка на крипту>."
-        "3. Отменить подписку на криптовалюту.\n"
-        "Для этого нажми на кнопку <Отменить подписку>."
-        "4. Изменить подписку на криптовалюту.\n"
-        "Для этого нажми на кнопку <Изменить подписку>."
-        )
-
-    await message.answer(
-        "Выберите:",
-        reply_markup=reply.help_kb.as_markup(resize_keyboard=True)
-    )
-
-
-@user_canal_router.message(F.text.strip().lower() == "подписка на крипту")
-async def follow_crypto(message: types.Message) -> types.Message:
-    await message.answer(text=
-        "Вы можете подписаться на определенную стоимость криптовалюты в USD, "
-        "и когда криптовалюта достигнет этой стоимости или перешагнет этот порог,"
-        " мы пришлем вам уведомление.\n"
-        "\n"
-        "Вы можете подписаться на 2 заданных значения: MIN и MAX.\n"
+        "Вы можете подписаться на 2 заданных вами значения: MIN и MAX.\n"
         "\n"
         "Если стоимость криптовалюты станет равна или больше MAX: "
-        "мы пришлем уведомление об этом.\n"
+        "я пришлю уведомление.\n"
         "Если стоимость криптовалюты станет равна или меньше MIN: "
-        "мы пришлем уведомление об этом.\n"
+        "я пришлю уведомление.\n"
         "\n"
-        "Чтобы подписаться на MAX стоимость криптовалюты, отправьте боту сообщение вида:\n"
-        "<code>MAX BTC 67000</code>\n"
-        "или\n"
-        "<code>MAX NOT 0.02</code>\n"
-        "\n"
-        "Чтобы подписаться на MIN стоимость криптовалюты, отправьте боту сообщение вида:\n"
-        "<code>MIN BTC 65000</code>\n"
-        "или\n"
-        "<code>MIN NOT 0.009</code>",
-        parse_mode=ParseMode.HTML
+        "Чтобы оформить подписку и управлять ей, "
+        "нажми на кнопку <Подписка на крипту>."
         )
+
+    await message.answer("Выберите:", reply_markup=reply.help_kb)
 
 
 @user_canal_router.message(F.text)
