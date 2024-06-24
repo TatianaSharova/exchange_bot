d662fafeee8e5e4f47e7c20adf879b9316fa7c4b
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-06-24 23:36:52 +0400

    deleted unuseful start_kb

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,19 +1,11 @@
 from aiogram import types
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-start_kb = types.ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            types.KeyboardButton(text="Криптовалюты"),
-            types.KeyboardButton(text="Помощь")
-        ]
-    ],
-    resize_keyboard=True,
-    one_time_keyboard=True
-    )
 
 crypto_kb = ReplyKeyboardBuilder()
-crypto_symbols = ['BTC', 'ETH', 'USDT', 'LTC', 'XLM', 'SOL', 'BNB', 'NOT', 'TON']
+crypto_symbols = [
+    'BTC', 'ETH', 'USDT', 'LTC', 'XLM', 'SOL', 'BNB', 'NOT', 'TON'
+]
 for crypto in crypto_symbols:
     crypto_kb.add(types.KeyboardButton(text=crypto))
 crypto_kb.adjust(3)
