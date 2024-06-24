08cea6aebf790e9703c1d49b38858805481cd631
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-06-24 23:37:10 +0400

    pep8

--- bot_cmds_list.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import BotCommand
-
-
-bot_cmds = [
-    BotCommand(command='subscribe', description='Подписка на крипту'),
-    BotCommand(command='check_price', description='Посмотреть стоимость криптовалют'),
-    BotCommand(command='help', description='Что может этот бот?'),
-    BotCommand(command='cancel', description='Отменить действие')
-]
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -5,37 +5,32 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def get_callback_btns(
     *,
     btns: dict[str, str],
-    sizes: tuple[int] = (2,)):
-
+    sizes: tuple[int] = (2,)
+):
     keyboard = InlineKeyboardBuilder()
 
     for text, data in btns.items():
-        
         keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
-
     return keyboard.adjust(*sizes).as_markup()
 
 
 def get_url_btns(
     *,
     btns: dict[str, str],
-    sizes: tuple[int] = (2,)):
-
+    sizes: tuple[int] = (2,)
+):
     keyboard = InlineKeyboardBuilder()
 
     for text, url in btns.items():
-        
         keyboard.add(InlineKeyboardButton(text=text, url=url))
-
     return keyboard.adjust(*sizes).as_markup()
 
 
-#Создать микс из CallBack и URL кнопок
-def get_inlineMix_btns(
+def get_inline_mix_btns(
     *,
     btns: dict[str, str],
-    sizes: tuple[int] = (2,)):
-
+    sizes: tuple[int] = (2,)
+):
     keyboard = InlineKeyboardBuilder()
 
     for text, value in btns.items():
